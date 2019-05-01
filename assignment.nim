c943306d08e0825abb23e2283c98de8a51057478
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-01 23:10:18 +0700

    add assignment

---
+++ assignment.nim
@@ -0,0 +1,18 @@
+# Simple assignment
+var a = 10
+var b: int = 15
+var
+  x: int = 10
+  y = 5.0
+  z = 'c'
+
+echo a, " ", b
+echo x, " ", y, " ", z
+
+# Compound assignment
+a += b
+b -= a
+echo a, " ", b
+a *= 2
+# b /= 5
+echo a, " ", b
