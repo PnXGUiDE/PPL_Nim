756eb5333d6239c189a9036e8e75f8737faa30ce
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-01 18:32:21 +0700

    fix enum

--- enumeration.nim
+++ enumeration.nim
@@ -12,8 +12,17 @@ var yesterday = sun
 if yesterday == Day.sun:
   echo "Yesterday is also Sunday!"
 
-
 # var tomorrow = today + 1
 var tomorrow = ord(today) + 1   # Use ordinal forms
 if tomorrow == ord(tue):
   echo "And, tomorrow will be Tuesday."
+
+type
+  Direction = enum
+    up = 2, down, left, right
+    # up, down, left = 30, right
+    # up = 2, down, left = 30, right
+    # up = 2, down, left = 1, right     # invalid order
+    # up, down, left = 1, right         # invalid order
+    
+echo ord(up), " ", ord(down), " ", ord(left), " ", ord(right)
