8ce4c6601fc71b886d2fc17a3179e0bf9211e743
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-01 22:42:36 +0700

    add shortcircuit

--- functionalSideEffect.nim
+++ functionalSideEffect.nim
@@ -1,9 +1,9 @@
 var a {.global.}: int = 3
 
-proc square(): int =
+proc someProc(): int =
   a = 5
   return 1
 
-a = a + square()    # Function first
-# a = square() + a
+a = a + someProc()    # Procedure first
+# a = someProc() + a
 echo a
