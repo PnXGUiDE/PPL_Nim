5aa10504fe39adc7b37b7164a8f6fe4d151d0a08
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-02 22:58:38 +0700

    edit switch

--- multipleSelection.nim
+++ multipleSelection.nim
@@ -20,3 +20,16 @@ case someInteger:
   of 0..3:      echo "Case A"
   of 4, 5, 6:   echo "Case B"
   else:         echo "n/a"
+
+import strutils   # import endsWith
+
+var animal = "white whale"
+var favoriteFood = case animal
+  of "dog": "bones"
+  of "cat": "mice"
+  elif animal.endsWith "whale": "plankton"
+  else:
+    echo "I'm not sure what to serve, but everybody loves ice cream"
+    "ice cream"
+
+echo favoriteFood
