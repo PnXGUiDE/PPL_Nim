bd3baa22561c18c97ec418585670ae8d1f82b84f
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-02 23:25:08 +0700

    add side effect pragma

---
+++ procedure.nim
@@ -0,0 +1,4 @@
+proc add(a, b: int): int =
+  result = a + b
+
+echo add(5, 3)
---
+++ sideEffectPragma.nim
@@ -0,0 +1,9 @@
+var x{.global.} = 10
+
+proc func1(): int =
+  x = 5
+  return 1
+
+proc func2(): int {.noSideEffect.} =
+  # x = 5
+  return 1
