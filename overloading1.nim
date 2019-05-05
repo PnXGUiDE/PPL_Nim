57adf87937aba2157bd43df43e450300cbc46cb8
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-05 19:09:47 +0700

    add overloading

--- overloading1.nim
+++ overloading1.nim
@@ -1,13 +1,10 @@
 proc add(a, b: int): int =
-  echo "float + float"
-  result = a + b
-
-proc add(): int =
   echo "int + int"
   result = a + b
 
+proc add(): int =
+  echo "<> + <>"
+  result = 3 + 2
 
+echo add()
 echo add(5, 4)
-echo add(5.0, 4)
-echo add(5, 4.0)
-echo add(5.0, 4.0)
